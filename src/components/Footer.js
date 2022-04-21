7d69e36e8967bedc0c5fb797ee06feeb9c3804d6
Yurii Nemchninov <57952185+L1keaFrost@users.noreply.github.com>
2022-04-21 15:20:05 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <>
-            <footer className="page-footer font-small navbar-dark bg-dark ">
+            <footer className="page-footer font-small navbar-dark bg-dark fixed-bottom">
                 <div className="container navbar-nav">
                     <div className="footer-copyright text-center py-3">
                         <a className="nav-link" href="https://github.com/L1keaFrost" target='_blank'> 2022 L1keAFrost</a>
